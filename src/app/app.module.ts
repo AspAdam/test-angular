0066cc595902a66c85a02bdaf41f1083dbb2ceb7
Aspengren <adam.aspengren@sogeti.com>
2022-01-28 16:10:50 +0100

    Some routing!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { Comp1Component } from './comp1/comp1.component';
+import { Comp2Component } from './comp2/comp2.component';
+import { Comp3Component } from './comp3/comp3.component';
+
+const appRoutes: Routes = [
+  { path: '1', component: Comp1Component },
+  { path: '2', component: Comp2Component },
+  { path: '3', component: Comp3Component },
+  { path: '',   redirectTo: '/1', pathMatch: 'full' },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    Comp1Component,
+    Comp2Component,
+    Comp3Component,
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
